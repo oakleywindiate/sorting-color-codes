2b1990f984ff669e849768a7687d73429214c175
Oakley Windiate <oakley.windiate@gmail.com>
2022-12-10 16:01:17 -0700

    Modify modal with symbol and styling

--- src/components/Correct.js
+++ src/components/Correct.js
@@ -2,20 +2,40 @@ import React, { useRef } from "react";
 import ReactDom from "react-dom";
 import { Link } from "react-router-dom";
 import './Modal.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons'
 
 const Correct = ({ setShowCorrectModal, nextQuestion}) => {
   const modalRef = useRef();
 
+  const correctPrompts = [
+    "Great!",
+    "Awesome!",
+    "Good job!",
+    "Correct!",
+    "Nice!",
+    "Excellent!"
+  ]
+
+  let randomCorrect = correctPrompts.sort(() => .5 - Math.random()).slice(0, 1);
+
   return ReactDom.createPortal(
-    <div className="container-correct" ref={modalRef}>
+    <div className="container-correct .modal-fade" ref={modalRef}>
       <div className="modal-correct">
-        <h2>CORRECT</h2>
-        <button className="next-button" onClick={() => {
-            setShowCorrectModal(false)
-            nextQuestion()
-          }
-        }>CONTINUE</button>
-      </div>
+        <div className="correct-left">
+          <FontAwesomeIcon className="faCircleCheck icon" icon={faCircleCheck} />
+          <div className="correct-text-div">
+            <h2 className="correct-text">{randomCorrect}</h2>
+          </div>
+        </div>
+        <div className="correct-right">
+          <button className="next-button-correct" onClick={() => {
+              setShowCorrectModal(false)
+              nextQuestion()
+            }
+          }>NEXT</button>
+        </div>
+      </div>  
     </div>,
     document.getElementById("portal")
   );
--- src/components/Incorrect.js
+++ src/components/Incorrect.js
@@ -2,19 +2,29 @@ import React, { useRef } from "react";
 import ReactDom from "react-dom";
 import { Link } from "react-router-dom";
 import './Modal.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons'
 
-const Incorrect = ({ setShowIncorrectModal, nextQuestion}) => {
+const Incorrect = ({ setShowIncorrectModal, nextQuestion, color }) => {
   const modalRef = useRef();
 
   return ReactDom.createPortal(
     <div className="container-incorrect" ref={modalRef}>
       <div className="modal-incorrect">
-        <h2>INCORRECT</h2>
-        <button className="next-button" onClick={() => {
-            setShowIncorrectModal(false)
-            nextQuestion()
-          }
-        }>CONTINUE</button>
+        <div className="incorrect-left">
+          <FontAwesomeIcon className="faCircleXmark icon" icon={faCircleXmark} />
+          <div className="incorrect-text-div">
+            <h2 className="incorrect-text">Correct Solution:</h2>
+            <h3 className="incorrect-color">{color}</h3>
+          </div>
+        </div>
+        <div className="incorrect-right">
+          <button className="next-button-incorrect" onClick={() => {
+              setShowIncorrectModal(false)
+              nextQuestion()
+            }
+          }>NEXT</button>
+        </div>
       </div>
     </div>,
     document.getElementById("portal")
